42b836cd8ebe7f233815fff220aaf1535715b4f6
Mirmakhmudov Abdulmajid <130051689+theMirmakhmudov@users.noreply.github.com>
2024-06-20 10:48:55 +0500

    Update buttons.py

--- buttons.py
+++ buttons.py
@@ -1,17 +1,10 @@
 from aiogram import types
 
-btn = [
-    [types.KeyboardButton(text="Feedback qoldirish")]
-
-]
+btn = [[types.KeyboardButton(text="Feedback qoldirish")]]
 button = types.ReplyKeyboardMarkup(keyboard=btn, resize_keyboard=True)
 
-contact_button = [
-    [types.KeyboardButton(text="Contact ulashish", request_contact=True)]
-]
+contact_button = [[types.KeyboardButton(text="Contact ulashish", request_contact=True)]]
 contact = types.ReplyKeyboardMarkup(keyboard=contact_button, resize_keyboard=True)
 
-location_button = [
-    [types.KeyboardButton(text="Location ulashish", request_location=True)]
-]
+location_button = [[types.KeyboardButton(text="Location ulashish", request_location=True)]]
 location = types.ReplyKeyboardMarkup(keyboard=location_button, resize_keyboard=True)
